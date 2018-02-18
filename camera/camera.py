e28d06ed1dbf11abed41fdedbde8e8f2182b2f63
yanzijun <yanzijun@me.com>
2018-02-18 18:23:13 +0000

    camera image xy, depth -> world xyz map

--- camera/camera.py
+++ camera/camera.py
@@ -1,10 +1,19 @@
 import numpy as np
 
 
-def camera_rotation_translation_to_R_and_T(translation, rotation):
+def camera_translation_rotation_to_R_and_T(translation, rotation):
     R = np.linalg.inv(rotation)
     T = -R @ translation
     return R, T
+
+
+def generate_RT_from_R_T(R, T):
+    RconT = np.concatenate((R, T), axis=1)
+    return np.concatenate((RconT, np.asmatrix([0, 0, 0, 1])), axis=0)
+
+
+def generate_K_from_mx_my_f_u_v(mx, my, f, u, v):
+    return np.asmatrix([[f * mx, 0, u, 0], [0, f * my, v, 0], [0, 0, 1, 0]])
 
 
 class Camera:
@@ -23,28 +32,23 @@ class Camera:
         self.T = T
 
         # Matrix for coordinate transformation
-        self.RT = self.generate_RT()
+        self.RT = generate_RT_from_R_T(self.R, self.T)
 
         # Matrix for perspective projection
-        self.K = self.generate_K()
+        self.K = generate_K_from_mx_my_f_u_v(self.mx, self.my, self.f, self.u, self.v)
 
         # Matrix and its inverse for transformation + perspective projection
         self.M = self.K @ self.RT
         self.M_pinv = np.linalg.pinv(self.M)
 
-    def generate_RT(self):
-        RconT = np.concatenate((self.R, self.T), axis=1)
-        return np.concatenate((RconT, np.asmatrix([0, 0, 0, 1])), axis=0)
-
-    def generate_K(self):
-        return np.asmatrix([[self.f * self.mx, 0, self.u, 0], [0, self.f * self.my, self.v, 0], [0, 0, 1, 0]])
+        self.cov = np.asmatrix([[0.01, 0, 0], [0, 0.01, 0], [0, 0, 0.01]])
 
     def generate_camera_position_orientation_from_R_T(self):
         R_inv = np.linalg.inv(self.R)
         return -R_inv @ self.T, R_inv
 
     def update_extrinsic_parameters_by_camera_position_orientation(self, position, orientation):
-        self.R, self.T = camera_rotation_translation_to_R_and_T(position, orientation)
+        self.R, self.T = camera_translation_rotation_to_R_and_T(position, orientation)
 
     def world_to_pixel(self, world_coordinate):
         ans = self.M @ world_coordinate
@@ -55,3 +59,15 @@ class Camera:
         ans = self.M_pinv @ pixel_coordinate
         return ans
 
+    def pixel_depth_to_world(self, pixel_coordinate, depth):
+        P, O = self.generate_camera_position_orientation_from_R_T()
+        Z = O @ np.asmatrix([[0], [0], [1]])
+        E = self.pixel_to_world(pixel_coordinate)[0:3]
+        D0 = (E - P)
+        D0 = D0 / np.linalg.norm(D0)
+        D = D0 * (-1 if (D0.T @ Z)[0][0] < 0 else 1)
+
+        return P + depth * D
+
+    def get_cov_by_depth(self, depth):
+        return depth * self.cov
--- camera/camera_test.py
+++ camera/camera_test.py
@@ -1,13 +1,19 @@
-import camera_configuaration.camera as camera1
+import camera as camera
 import numpy as np
 
 # ground measure
 # obj: 0.19  dist: 0.235 foc: 0.035 pix: 2448 mx: 86500
 # this calculation is terrible, just for trail
 
+# TODO: this is the most vulnerable, need more test
+
 R = np.asmatrix([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
-T = np.asmatrix([[1], [1], [1]])
-camera1 = camera1.Camera(86500, 86500, 0.035, 2448 / 2, 3264 / 2, R, T)
+T = np.asmatrix([[0], [0], [0]])
+camera1 = camera.Camera(86500, 86500, 0.035, 2448 / 2, 3264 / 2, R, T)
+
+print('K,RT check')
+print(camera1.K)
+print(camera1.RT)
 
 coordinate_world = np.asmatrix([[-0.095], [-0.095], [0.235], [1]])
 coordinate_pixel = camera1.world_to_pixel(coordinate_world)
@@ -18,10 +24,15 @@ print(coordinate_pixel)
 coordinate_world_back = camera1.pixel_to_world(coordinate_pixel)
 # coordinate_world_back[3] = 0
 
+coordinate_world_with_depth = camera1.pixel_depth_to_world(coordinate_pixel, 0.235)
+
+print('coordinate_world_with_depth')
+print(coordinate_world_with_depth)
+
 print(coordinate_world_back)
 
 coordinate_pixel_again = camera1.world_to_pixel(coordinate_world_back)
 
 print(coordinate_pixel_again)
 
-print(camera1.generate_camera_position_orientation_from_R_T()[0])
+print(camera1.generate_camera_position_orientation_from_R_T()[1])
