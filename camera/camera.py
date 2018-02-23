d27b64a3dd212043420364ae715b870896a7cef1
yanzijun <yanzijun@me.com>
2018-02-23 19:33:46 +0000

    new extrinsic parameter update by position and direction

--- camera/camera.py
+++ camera/camera.py
@@ -1,22 +1,46 @@
 import numpy as np
 
 
-def camera_translation_rotation_to_R_and_T(translation, rotation):
-    R = np.linalg.inv(rotation)
-    T = -R @ translation
-    return R, T
-
-
-def generate_RT_from_R_T(R, T):
-    RconT = np.concatenate((R, T), axis=1)
-    return np.concatenate((RconT, np.asmatrix([0, 0, 0, 1])), axis=0)
-
-
-def generate_K_from_mx_my_f_u_v(mx, my, f, u, v):
-    return np.asmatrix([[f * mx, 0, u, 0], [0, f * my, v, 0], [0, 0, 1, 0]])
-
-
 class Camera:
+    @staticmethod
+    def camera_position_rotation_to_R_T(position, rotation):
+        R = np.linalg.inv(rotation)
+        T = -R @ position
+        return R, T
+
+    @staticmethod
+    def camera_position_direction_to_R_T(position, direction):
+        rotation = Camera.generate_rotation_from_direction(direction)
+        return Camera.camera_position_rotation_to_R_T(position, rotation)
+
+    @staticmethod
+    def generate_RT_from_R_T(R, T):
+        RconT = np.concatenate((R, T), axis=1)
+        return np.concatenate((RconT, np.asmatrix([0, 0, 0, 1])), axis=0)
+
+    @staticmethod
+    def generate_K_from_mx_my_f_u_v(mx, my, f, u, v):
+        return np.asmatrix([[f * mx, 0, u, 0], [0, f * my, v, 0], [0, 0, 1, 0]])
+
+    @staticmethod
+    def generate_rotation_from_direction(D):
+        D = D / np.linalg.norm(D)
+        Z = np.asmatrix([[0.0], [0.0], [1.0]])
+        C = Z.T @ D
+        V = np.cross(Z.flatten(), D.flatten()).reshape((3, 1))
+        Vx = np.asmatrix([[0.0, -V[2, 0], V[1, 0]],
+                          [V[2, 0], 0.0, -V[0, 0]],
+                          [-V[1, 0], V[0, 0], 0.0]])
+        I = np.asmatrix([[1.0, 0.0, 0.0],
+                         [0.0, 1.0, 0.0],
+                         [0.0, 0.0, 1.0]])
+        # S = np.linalg.norm(V)
+        if C != -1.0:
+            R_inv = I + Vx + Vx @ Vx / (1 + C)
+        else:
+            R_inv = -I
+        return R_inv
+
     def __init__(self, mx, my, f, u, v, R, T):
         # Intrinsic parameters
         self.mx = mx
@@ -32,10 +56,10 @@ class Camera:
         self.T = T
 
         # Matrix for coordinate transformation
-        self.RT = generate_RT_from_R_T(self.R, self.T)
+        self.RT = Camera.generate_RT_from_R_T(self.R, self.T)
 
         # Matrix for perspective projection
-        self.K = generate_K_from_mx_my_f_u_v(self.mx, self.my, self.f, self.u, self.v)
+        self.K = Camera.generate_K_from_mx_my_f_u_v(self.mx, self.my, self.f, self.u, self.v)
 
         # Matrix and its inverse for transformation + perspective projection
         self.M = self.K @ self.RT
@@ -47,11 +71,15 @@ class Camera:
         R_inv = np.linalg.inv(self.R)
         return -R_inv @ self.T, R_inv
 
-    def update_extrinsic_parameters_by_camera_position_orientation(self, position, orientation):
-        self.R, self.T = camera_translation_rotation_to_R_and_T(position, orientation)
-        self.RT = generate_RT_from_R_T(self.R, self.T)
+    def update_extrinsic_parameters_by_camera_position_rotation(self, position, rotation):
+        self.R, self.T = Camera.camera_position_rotation_to_R_T(position, rotation)
+        self.RT = Camera.generate_RT_from_R_T(self.R, self.T)
         self.M = self.K @ self.RT
         self.M_pinv = np.linalg.pinv(self.M)
+
+    def update_extrinsic_parameters_by_camera_position_direction(self, position, direction):
+        rotation = Camera.generate_rotation_from_direction(direction)
+        self.update_extrinsic_parameters_by_camera_position_rotation(position, rotation)
 
     def world_to_pixel(self, world_coordinate):
         ans = self.M @ world_coordinate
@@ -63,22 +91,22 @@ class Camera:
         return ans
 
     def pixel_depth_to_world(self, pixel_coordinate, depth):
-        print('pixel coordinate: ', pixel_coordinate)
-        print('depth', depth)
+        # print('pixel coordinate: ', pixel_coordinate)
+        # print('depth', depth)
         P, O = self.generate_camera_position_orientation_from_R_T()
-        print('camera position:', P)
+        # print('camera position:', P)
         Z = O @ np.asmatrix([[0], [0], [1]])
         E4 = self.pixel_to_world(pixel_coordinate)
         # TODO: argue that if the E_4 is 0, the camera position must be at 0
         E3 = E4[0:3] / (E4[3][0] if E4[3][0] != 0 else 1.0)
-        print('E3', E3)
-        print('E4', E4)
+        # print('E3', E3)
+        # print('E4', E4)
         D0 = (E3 - P)
         D0 = D0 / np.linalg.norm(D0)
         D = D0 * (-1 if (D0.T @ Z)[0][0] < 0 else 1)
-        print('D: ', D)
+        # print('D: ', D)
         world_coordinate = P + depth * D
-        print('world coordinate: ', world_coordinate)
+        # print('world coordinate: ', world_coordinate)
         return world_coordinate
 
     def get_cov_by_depth(self, depth):
--- camera/camera_test.py
+++ camera/camera_test.py
@@ -1,5 +1,36 @@
-import camera as camera
+import camera as camera_
 import numpy as np
+
+print('---TEST---def generate_R_inv_from_direction(D)---BEGIN---')
+Z = np.asmatrix([[0.0], [0.0], [1.0]])
+test_case = [np.asmatrix(np.random.normal(1, 1, (3, 1))) for _ in range(10)]
+print('test case')
+print(test_case)
+
+test_case.append(-Z)
+R_invs = []
+validation_ground_truth = []
+for A in test_case:
+    R_invs.append(camera_.Camera.generate_rotation_from_direction(A))
+    validation_ground_truth.append(A / np.linalg.norm(A))
+validation = [R_invs[i] @ Z - validation_ground_truth[i] for i in range(len(R_invs))]
+print(validation)
+print('---TEST---def generate_R_inv_from_direction(D)---END---')
+
+print('---TEST---Camera---BEGIN---')
+direction = np.asmatrix([[1], [1], [1]])
+position = np.asmatrix([[13], [13], [13]])
+R = np.asmatrix([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
+T = np.asmatrix([[10], [10], [10]])
+camera = camera_.Camera(86500, 86500, 0.035, 3264 / 2, 2448 / 2, R, T)
+camera.update_extrinsic_parameters_by_camera_position_direction(position, direction)
+print(camera.R)
+print(camera.M)
+depth = 1
+pixel_coordinate = np.asmatrix([[1632], [1224], [1.0]])
+world_coordinate = camera.pixel_depth_to_world(pixel_coordinate, depth)
+print(world_coordinate)
+print('---TEST---Camera---END---')
 
 # ground measure
 # obj: 0.19  dist: 0.235 foc: 0.035 pix: 2448 mx: 86500
@@ -7,32 +38,32 @@ import numpy as np
 
 # TODO: this is the most vulnerable, need more test
 
-R = np.asmatrix([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
-T = np.asmatrix([[10], [10], [10]])
-camera1 = camera.Camera(86500, 86500, 0.035, 2448 / 2, 3264 / 2, R, T)
-
-print('K,RT check')
-print(camera1.K)
-print(camera1.RT)
-
-coordinate_world = np.asmatrix([[-0.095], [-0.095], [0.235], [1]])
-coordinate_pixel = camera1.world_to_pixel(coordinate_world)
-coordinate_pixel /= coordinate_pixel[2]
-
-print(coordinate_pixel)
-
-coordinate_world_back = camera1.pixel_to_world(coordinate_pixel)
-# coordinate_world_back[3] = 0
-
-coordinate_world_with_depth = camera1.pixel_depth_to_world(coordinate_pixel, 0.235)
-
-print('coordinate_world_with_depth')
-print(coordinate_world_with_depth)
-
-print(coordinate_world_back)
-
-coordinate_pixel_again = camera1.world_to_pixel(coordinate_world_back)
-
-print(coordinate_pixel_again)
-
-print(camera1.generate_camera_position_orientation_from_R_T()[1])
+# R = np.asmatrix([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
+# T = np.asmatrix([[10], [10], [10]])
+# camera1 = camera_.Camera(86500, 86500, 0.035, 2448 / 2, 3264 / 2, R, T)
+#
+# print('K,RT check')
+# print(camera1.K)
+# print(camera1.RT)
+#
+# coordinate_world = np.asmatrix([[-0.095], [0.095], [0.235], [1]])
+# coordinate_pixel = camera1.world_to_pixel(coordinate_world)
+# coordinate_pixel /= coordinate_pixel[2]
+#
+# print(coordinate_pixel)
+#
+# coordinate_world_back = camera1.pixel_to_world(coordinate_pixel)
+# # coordinate_world_back[3] = 0
+#
+# coordinate_world_with_depth = camera1.pixel_depth_to_world(coordinate_pixel, 100)
+#
+# print('coordinate_world_with_depth')
+# print(coordinate_world_with_depth)
+#
+# print(coordinate_world_back)
+#
+# coordinate_pixel_again = camera1.world_to_pixel(coordinate_world_back)
+#
+# print(coordinate_pixel_again)
+#
+# print(camera1.generate_camera_position_orientation_from_R_T()[1])
