2ec31fa26484a9d5ddae16b6b07a70f55bc8c802
yanzijun <yanzijun@me.com>
2018-02-25 01:01:05 +0000

    interface for depth/motion detector

---
+++ detector/depth_detector.py
@@ -0,0 +1,6 @@
+class DepthDetector:
+    def __init__(self):
+        return
+
+    def detect_depth(self, imageL, imageR):
+        return
---
+++ detector/motion_detector.py
@@ -0,0 +1,6 @@
+class MotionDetector:
+    def __init__(self):
+        return
+
+    def detect_depth(self, image_prev, image_curr):
+        return
