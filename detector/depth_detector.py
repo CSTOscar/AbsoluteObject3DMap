b119fae27e805daaec17bd0b3fe54e32c12da0f8
Yifan Bai <yifan.f.bai@gmail.com>
2018-03-03 17:02:58 +0000

    depth detector built with reprojectImageTo3d; POOR performance

--- detector/depth_detector.py
+++ detector/depth_detector.py
@@ -100,10 +100,123 @@ def detect_depth(frame):
     # cv2.resizeWindow('morphology', 1200, 900)
     # cv2.imshow('morphology', morphology)
 
+    depth_map = np.delete(image_3d, [0,1], axis=2)
+    # print(depth_map)
+    # depth_map = np.nan_to_num(depth_map)
+    # depth_map *= 255.0 / np.max(depth_map)
+    # depth_map = np.uint8(depth_map)
+    #
+    # cv2.namedWindow('depth map', cv2.WINDOW_NORMAL)
+    # cv2.resizeWindow('depth map', 1200, 900)
+    # cv2.imshow('depth map', depth_map)
+
+    print("done")
+    cv2.waitKey(0)
+
+    return depth_map
+
+
+
+def detect_depth_image_3D(frame):
+
+    imageL = frame.imageL
+    imageR = frame.imageR
+
+    # disparity settings
+    window_size = 5
+    min_disp = 1
+    num_disp = 129 - min_disp
+
+    matcher_left = cv2.StereoSGBM_create(
+        minDisparity=0,
+        numDisparities=64,
+        blockSize=15,
+        P1=8 * 3 * window_size ** 2,
+        P2=32 * 3 * window_size ** 2,
+        disp12MaxDiff=1,
+        uniquenessRatio=15,
+        speckleWindowSize=200,
+        speckleRange=2,
+        # preFilterCap=63,
+        mode=cv2.STEREO_SGBM_MODE_SGBM_3WAY
+    )
+
+    matcher_right = cv2.ximgproc.createRightMatcher(matcher_left=matcher_left)
+
+    # filter parameters
+    lambda_ = 80000
+    sigma = 1.2
+    wls_filter = cv2.ximgproc.createDisparityWLSFilter(matcher_left=matcher_left)
+    wls_filter.setLambda(_lambda=lambda_)
+    wls_filter.setSigmaColor(sigma)
+
+    imageL_bw = cv2.cvtColor(imageL, cv2.COLOR_RGB2GRAY)
+    imageR_bw = cv2.cvtColor(imageR, cv2.COLOR_RGB2GRAY)
+
+    # stereo = cv2.StereoBM_create(numDisparities=64, blockSize=15)
+
+
+    # cv2.imshow('left eye', image_left)
+
+    # compute disparity
+    # disparity = matcher_left.compute(imageL, imageR).astype(np.float32)
+    # disparity = (disparity - min_disp) / num_disp
+
+    print('computing disparity...')
+    displ = matcher_left.compute(imageL_bw, imageR_bw)
+    dispr = matcher_right.compute(imageR_bw, imageL_bw)  # .astype(np.float32)/16
+    displ = np.int16(displ)
+    dispr = np.int16(dispr)
+    # filter the image
+    filteredImg = wls_filter.filter(displ, imageL, None, dispr)
+    # filteredImg = cv2.normalize(src=filteredImg, dst=filteredImg, beta=0, alpha=255, norm_type=cv2.NORM_MINMAX)
+
+    Q_matrix = None
+
+    # parameter needed for the reprojection
+    img_shape = imageL.shape[:2]
+    rotation_matrix_input = np.float64(frame.camera.R)
+    translation_matrix_input = np.float64(frame.camera.T)
+    camera_intrinsics = np.delete(frame.camera.K, 3, 1)
+    camera_distortion = np.array(frame.camera.Dist)
+
+    Q_matrix = cv2.stereoRectify(camera_intrinsics, camera_distortion, camera_intrinsics, camera_distortion, img_shape,
+                      rotation_matrix_input,
+                      translation_matrix_input)[4]
+
+    image_3d = cv2.reprojectImageTo3D(np.array(filteredImg), Q=Q_matrix)
+
+    # print(np.min(filteredImg), " ", np.max(filteredImg), " ", np.average(filteredImg))
+
+    filteredImg = np.uint8(filteredImg)
+
+    # morphology settings
+    # kernel = np.ones((3, 3), np.uint8)
+    # apply morphological transformation
+    # morphology = cv2.morphologyEx(disparity, cv2.MORPH_OPEN, kernel)
+
+    # show images
+
+    # cv2.namedWindow('left eye', cv2.WINDOW_NORMAL)
+    # cv2.resizeWindow('left eye', 1200, 900)
+    # cv2.imshow('left eye', imageL)
+    #
+    # cv2.namedWindow('right eye', cv2.WINDOW_NORMAL)
+    # cv2.resizeWindow('right eye', 1200, 900)
+    # cv2.imshow('right eye', imageR)
+    #
+    # cv2.namedWindow('disparity', cv2.WINDOW_NORMAL)
+    # cv2.resizeWindow('disparity', 1200, 900)
+    # cv2.imshow('disparity', filteredImg)
+
+
+    # cv2.namedWindow('morphology', cv2.WINDOW_NORMAL)
+    # cv2.resizeWindow('morphology', 1200, 900)
+    # cv2.imshow('morphology', morphology)
+
+
     print("done")
     cv2.waitKey(0)
 
     return image_3d
-
-
 
--- test/depth_detect/depth_detector_test_new.py
+++ test/depth_detect/depth_detector_test_new.py
@@ -1,10 +1,39 @@
-from detector.depth_detector import detect_depth
+from detector.depth_detector import detect_depth, detect_depth_image_3D
 from frame.frame import generate_raw_frame_chain_from_images
 from camera.camera import Camera
 import numpy as np
 import cv2
 from pyntcloud import PyntCloud
 import pandas as pd
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from itertools import cycle
+from laspy.file import File
+
+
+# def write_xyz(fout, coords, title="", atomtypes=("A",)):
+#     """ write a xyz file from file handle
+#     Writes coordinates in xyz format. It uses atomtypes as names. The list is
+#     cycled if it contains less entries than there are coordinates,
+#     One can also directly write xyz data which was generated with read_xyz.
+#     # >>> xx = read_xyz("in.xyz")
+#     # >>> write_xyz(open("out.xyz", "w"), *xx)
+#     Parameters
+#     ----------
+#     fout : an open file
+#     coords : np.array
+#         array of coordinates
+#     title : title section, optional
+#         title for xyz file
+#     atomtypes : iteratable
+#         list of atomtypes.
+#     See Also
+#     --------
+#     read_xyz
+#     """
+#     fout.write("%d\n%s\n" % (coords.size / 3, title))
+#     for x, atomtype in zip(coords.reshape(-1, 3), cycle(atomtypes)):
+#         fout.write("%s %.18g %.18g %.18g\n" % (atomtype, x[0], x[1], x[2]))
 
 
 if __name__ == '__main__':
@@ -19,11 +48,30 @@ if __name__ == '__main__':
         camera_input.calibrate_by_images_and_grid_length(image_cali, 0.014)
         frame_input = generate_raw_frame_chain_from_images([image_left], [image_right], camera_input)
         # get 3d points
-        image_3d = np.array(detect_depth(frame_input[0]))
-        # turn them into point cloud
-        point_list = pd.DataFrame(image_3d.reshape((image_3d.shape[0] * image_3d.shape[1], 3)))
-        point_list.columns = ['x', 'y', 'z']
-        # cloud = PyntCloud(points=point_list)
-        # cloud.to_file("test1.ply")
+        # image_3d = np.array(detect_depth_image_3D(frame_input[0]))
+        # reshaped_img = image_3d.reshape((image_3d.shape[0] * image_3d.shape[1], 3))
 
-        print(point_list)
+        # depth map
+        depth_map  = detect_depth(frame_input[0])
+
+
+        # # turn them into point cloud
+        # point_list = pd.DataFrame(image_3d.reshape((image_3d.shape[0] * image_3d.shape[1], 3)))
+        # point_list.columns = ['x', 'y', 'z']
+        #
+        #
+        #
+        #
+        # # with open('test.xyz', 'w') as f:
+        # #     write_xyz(f, reshaped_img, ('A','B'))
+        #
+        #
+        # # cloud = PyntCloud(points=point_list)
+        # # cloud.to_file("test1.ply")
+        #
+        # print(point_list)
+
+
+
+
+
