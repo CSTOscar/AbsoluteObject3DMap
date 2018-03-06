f9ec9098341928a687eb80fd308db333ce97f261
yanzijun <yanzijun@me.com>
2018-03-06 13:20:14 +0000

    new main for recollect objects

--- main/main.py
+++ main/main.py
@@ -5,10 +5,12 @@ from frame import frame as fm
 from world_model import world as wd
 from camera import camera as cm
 from detector import object_detector as od
+from PIL import Image
 
-cwd = os.path.dirname(os.path.realpath(__file__)) 
+cwd = os.path.dirname(os.path.realpath(__file__))
 
-CKPT_FILE_PATH = os.path.abspath(cwd + '/../data/model_files/ssd_mobilenet_v1_coco_2017_11_17/frozen_inference_graph.pb')
+CKPT_FILE_PATH = os.path.abspath(
+    cwd + '/../data/model_files/ssd_mobilenet_v1_coco_2017_11_17/frozen_inference_graph.pb')
 LABEL_FILE_PATH = os.path.abspath(cwd + '/../data/label_files/mscoco_label_map.pbtxt')
 NUM_CLASS = 90
 
@@ -17,13 +19,17 @@ K = None
 camera = None
 
 
-def setup():
+def setup(K0=None):
     global K
     global camera
 
-    K = np.asmatrix([[1.12265949e+03, 0.00000000e+00, 3.07322388e+02, 0.00000000e+00],
-                     [0.00000000e+00, 1.11948374e+03, 5.50850887e+02, 0.00000000e+00],
-                     [0.00000000e+00, 0.00000000e+00, 1.00000000e+00, 0.00000000e+00]])
+    # K = np.asmatrix([[1.12265949e+03, 0.00000000e+00, 3.07322388e+02, 0.00000000e+00],
+    #                  [0.00000000e+00, 1.11948374e+03, 5.50850887e+02, 0.00000000e+00],
+    #                  [0.00000000e+00, 0.00000000e+00, 1.00000000e+00, 0.00000000e+00]])
+    if K0 is not None:
+        K = np.asmatrix([[2.01078286e+03, 0.00000000e+00, 4.93720699e+02, 0.00000000e+00],
+                         [0.00000000e+00, 2.02736414e+03, 1.01140977e+03, 0.00000000e+00],
+                         [0.00000000e+00, 0.00000000e+00, 1.00000000e+00, 0.00000000e+00]])
     R = [[1, 0, 0], [0, 1, 0], [0, 0, 1]]
     T = [[1], [1], [1]]
     camera = cm.Camera(86500, 86500, 0.035, 3264 / 2, 2448 / 2, R, T)
@@ -34,7 +40,10 @@ def setup():
     detector = od.ObjectorDetector(CKPT_FILE_PATH, LABEL_FILE_PATH, NUM_CLASS)
 
 
-def main(imagesL, imagesR):
+frames_cache = None
+
+
+def main(imagesL, imagesR, confidence=0.2):
     # set up the initial cameras 
     global camera
     global detector
@@ -47,32 +56,68 @@ def main(imagesL, imagesR):
     fm.generate_set_detection_info_in_frame_chain(frame_list, detector)
     fm.generate_set_projections_in_frame_chain(frame_list)
 
+    global frames_cache
+    frames_cache = frame_list
+
     world = wd.World()
-    world.add_projections(fm.collect_projections_from_frames(frame_list))
+    projections = fm.collect_projections_from_frames(frame_list, confidence=confidence)
+    world.add_projections(projections)
     world.unify_objects_projection_get_object()
 
+    # CHECK_IMAGE_PATH_FORMAT = '/Users/zijunyan/Desktop/Oscar/AbsoluteObject3DMap/data/image_files/check_image/image{}.jpg'
+    # images = fm.object_depth_detection_check_plot(frame_list, confidence=confidence)
+    # for i, image in enumerate(images):
+    #     print('saving image: ', i)
+    #     im = Image.fromarray(image)
+    #     im.save(CHECK_IMAGE_PATH_FORMAT.format(i))
     return world.get_json()
+
+
+def get_objects_again(confidence):
+    global frames_cache
+    if frames_cache is not None:
+        projections = fm.collect_projections_from_frames(frames_cache, confidence)
+        world = wd.World()
+        world.add_projections(projections)
+        world.unify_objects_projection_get_object()
+        return world.get_json()
+
+
+def convert_portrait(image):
+    print(type(image))
+
+    shape = image.shape
+    print(shape)
+    if shape[0] < shape[1]:
+        image = np.rot90(image, -1, axes=(0, 1))
+        print(image.shape)
+    return image
 
 
 def test():
     from video_process import video_process
-    VIDEO_PATH_L = '/Users/zijunyan/Desktop/Oscar/AbsoluteObject3DMap/data/video_files/test_left.mp4'
-    VIDEO_PATH_R = '/Users/zijunyan/Desktop/Oscar/AbsoluteObject3DMap/data/video_files/test_right.mp4'
+    VIDEO_PATH_L = '/Users/zijunyan/Downloads/new test/left.mp4'
+    VIDEO_PATH_R = '/Users/zijunyan/Downloads/new test/right.mp4'
     VIDEO_PATH_C = '/Users/zijunyan/Downloads/video_cal.mp4'
     FRAME_DIR_PATH = '../../data/image_files'
-    STEP = 30
+    STEP = 20
 
     # images_cal = video_process.capture_frames_from_video(VIDEO_PATH_C, 5)
 
     imagesL = video_process.capture_frames_from_video(VIDEO_PATH_L, STEP)
     imagesR = video_process.capture_frames_from_video(VIDEO_PATH_R, STEP)
 
-    setup()
-    imagesLR = []
-    for i in range(min(len(imagesL), len(imagesR))):
-        imagesLR.append((imagesL[i], imagesR[i]))
+    imgL = []
+    imgR = []
 
-    json = main(imagesLR)
+    for i, image in enumerate(imagesL):
+        imgL.append(convert_portrait(image))
+
+    for i, image in enumerate(imagesR):
+        imgR.append(convert_portrait(image))
+
+    setup()
+    json = main(imgL, imgR)
 
     print(json)
 
@@ -80,5 +125,4 @@ def test():
     # fm.object_depth_detection_check_plot()
 
 
-# test()
-
+test()
